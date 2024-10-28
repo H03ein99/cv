85490a29c0ad7e78aaa3511556bd1a0b269839e9
H03ein99 <hosseingh539@gmail.com>
2024-10-28 17:56:33 +0330

    modify template
    modify template
    +deactivated about_view
    +deactivated contact_view

--- MyCv/website/views.py
+++ MyCv/website/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 def index_view(request):
     return render(request, 'website/index.html')
 
-def about_view(request):
-    return render(request, 'website/about-us.html')
+# def about_view(request):
+#     return render(request, 'website/about-us.html')
 
-def contact_view(request):
-    return render(request, 'website/contact.html')
+# def contact_view(request):
+#     return render(request, 'website/contact.html')
